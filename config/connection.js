89a799d236765e23d77800dbd69d147d35e71e40
MrJoggington <49696148+MrJoggington@users.noreply.github.com>
2019-11-25 16:30:53 -0800

    heroku test 4

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,6 @@ if (process.env.JAWSDB_URL) {
         user: "root",
         password: "Smitty#1",
         database: "burgers_db",
-        database_url: "postgres://slmiosrcfkriyu:728fdb54cf584f76059992963c534da297bd4e5f13553a64c0b67618fb9ecc81@ec2-54-225-173-42.compute-1.amazonaws.com:5432/d6ud5pm2a28ke4"
     });
 };
 
